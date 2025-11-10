bd3d1157fc390b362e8dfe8774cfc5cafae17ca0
Jon Welfringer <7365166+W6HBR@users.noreply.github.com>
2025-11-10 08:27:21 -0800

    Refactor ServerStartup for SignalR and response compression
    Cleaned up with suggestions from @mdmontesinos

--- SignalR.Module.Chat/Server/Startup/ServerStartup.cs
+++ SignalR.Module.Chat/Server/Startup/ServerStartup.cs
@@ -1,5 +1,3 @@
-//using BlazorSignalRApp.Client.Pages;
-//using BlazorSignalRApp.Components;
 using Microsoft.AspNetCore.Builder; 
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
@@ -13,73 +11,16 @@ using SignalR.Module.Chat.Services;
 using System.Linq;
 
 
-using System.Net.Http;
-using System.Net.Http.Json;
-using Microsoft.AspNetCore.Components.Forms;
-using Microsoft.AspNetCore.Components.Routing;
-using Microsoft.AspNetCore.Components.Web;
-using static Microsoft.AspNetCore.Components.Web.RenderMode;
-using Microsoft.AspNetCore.Components.Web.Virtualization;
-using Microsoft.JSInterop;
-
-
-
 namespace SignalR.Module.Chat.Startup
 {
     public class ServerStartup : IServerStartup
     {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            var builder = WebApplication.CreateBuilder();
-
-            // Add services to the container.
-            builder.Services.AddRazorComponents()
-                .AddInteractiveWebAssemblyComponents();
-
-            builder.Services.AddSignalR();
-
-            builder.Services.AddResponseCompression(opts =>
-            {
-                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
-                    ["application/octet-stream"]);
-            });
-
-            //var app = builder.Build();
-            app = builder.Build();
-
-            app.UseResponseCompression();
-
-            // Configure the HTTP request pipeline.
-            /*
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            */
-            {
-                app.UseExceptionHandler("/Error", createScopeForErrors: true);
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-
-            //app.MapStaticAssets();
-            app.UseAntiforgery();
-
-            //app.MapRazorComponents<App>()
-            //    .AddInteractiveWebAssemblyRenderMode();
-           //     .AddAdditionalAssemblies(typeof(BlazorSignalRApp.Client._Imports).Assembly);
-
-            app.UseRouting();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapHub<ChatHub>("/chathub");
-                //endpoints.MapControllers();
-                //endpoints.MapFallback();
             });
-            //app.MapHub<ChatHub>("/chathub");
 
         }
 
@@ -92,6 +33,14 @@ namespace SignalR.Module.Chat.Startup
         {
             services.AddTransient<IChatService, ServerChatService>();
             services.AddDbContextFactory<ChatContext>(opt => { }, ServiceLifetime.Transient);
+            
+            services.AddSignalR();
+            
+            services.AddResponseCompression(opts =>
+            {
+                opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
+                    ["application/octet-stream"]);
+            });        
         }
     }
 }
